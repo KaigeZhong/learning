243ae481a1e87221620db2e19d5018997829c346
kaige zhong <kaige.zhong@isinonet.com>
2018-07-16 00:52:17 +0800

    policy~

--- jdk/basic/src/main/java/com/policy/PolicyLearning.java
+++ jdk/basic/src/main/java/com/policy/PolicyLearning.java
@@ -8,5 +8,7 @@ public class PolicyLearning {
     System.setProperty("java.security.policy", policy);
     System.setSecurityManager(new SecurityManager());
     FileUtil.createFile("/home/kaige/work/policy/file_a");
+//    FileUtil.createFile("/home/kaige/work/policy_read_only/file_a");
+    FileUtil.doPrivilegedAction("/home/kaige/work/policy_read_only/file_a");
   }
 }
--- jdk/basic/src/main/java/com/policy/utils/FileUtil.java
+++ jdk/basic/src/main/java/com/policy/utils/FileUtil.java
@@ -2,6 +2,8 @@ package com.policy.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
 public class FileUtil {
   public static void createFile(String name) {
@@ -11,5 +13,16 @@ public class FileUtil {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  public static void doPrivilegedAction(final String fileName) {
+    // 用特权访问方式创建文件
+    AccessController.doPrivileged(new PrivilegedAction<String>() {
+      @Override
+      public String run() {
+        createFile(fileName);
+        return null;
+      }
+    });
   }
 }
