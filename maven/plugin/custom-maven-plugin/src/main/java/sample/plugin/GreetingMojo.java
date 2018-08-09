81e2ad61e8d87380f9ce4d41662f4de2ce0fac54
Kaige Zhong <zhongkaige@xinchao.com>
2018-08-09 11:17:11 +0800

    add config

--- maven/plugin/custom-maven-plugin/src/main/java/sample/plugin/GreetingMojo.java
+++ maven/plugin/custom-maven-plugin/src/main/java/sample/plugin/GreetingMojo.java
@@ -3,16 +3,34 @@ package sample.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * Says "Hi" to the user.
  */
-@Mojo(name = "sayhi")
+@Mojo(name = "sayHi")
 public class GreetingMojo extends AbstractMojo {
+    @Parameter(property = "configKey")
+    private String configKey;
+    @Parameter
+    private String config;
+    @Parameter(property = "configKeyWithDefault", defaultValue = "configDefaultValue")
+    private String configKeyWithDefault;
+
     public void execute() throws MojoExecutionException {
         getLog().info(
                 "######################################################" +
                         "Hello, world." +
+                        "######################################################");
+        logParameter();
+    }
+
+    private void logParameter() {
+        getLog().info(
+                "######################################################" +
+                        "configKey: " + configKey + ", " +
+                        "config: " + config +
+                        "configKeyWithDefault: " + configKeyWithDefault + ", " +
                         "######################################################");
     }
 }
