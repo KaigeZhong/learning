1716e404657f399243d909f87954581e17cd606a
Kaige Zhong <zhongkaige@xinchao.com>
2018-09-05 12:31:30 +0800

    cloud config

---
+++ spring/spring-cloud-learning/spring-cloud-config-client/src/main/java/com/ConfigClient.java
@@ -0,0 +1,26 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient {
+
+    private String foo;
+    private String testKey;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient.class, args);
+    }
+
+    @Value("${foo}")
+    public void setFoo(String foo) {
+        this.foo = foo;
+    }
+
+    @Value("${testKey}")
+    public void setTestKey(String testKey) {
+        this.testKey = testKey;
+    }
+}
