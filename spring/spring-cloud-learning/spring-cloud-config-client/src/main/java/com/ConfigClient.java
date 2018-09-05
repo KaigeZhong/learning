b7e1a6050c5f34d88b7d245451673bc3ac3be82b
Kaige Zhong <zhongkaige@xinchao.com>
2018-09-05 18:16:22 +0800

    ll

--- spring/spring-cloud-learning/spring-cloud-config-client/src/main/java/com/ConfigClient.java
+++ spring/spring-cloud-learning/spring-cloud-config-client/src/main/java/com/ConfigClient.java
@@ -1,19 +1,39 @@
 package com;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ConfigClient {
+@RestController
+public class ConfigClient implements ApplicationContextAware {
 
     private String foo;
     private String testKey;
     private String bootKey;
     private String customKey;
+    private ApplicationContext context;
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClient.class, args);
+    }
+
+    @RequestMapping("/")
+    public String testKeyValue() {
+        String testValue = value("testKey");
+        return testValue;
+    }
+    @RequestMapping("/{key}")
+    public String value(@PathVariable String key) {
+        String property = context.getEnvironment().getProperty(key);
+        String propertyFromParent = context.getParent().getEnvironment().getProperty(key);
+        return property + ", " + propertyFromParent;
     }
 
     @Value("${foo}")
@@ -34,5 +54,9 @@ public class ConfigClient {
     @Value("${customKey}")
     public void setCustomKey(String customKey) {
         this.customKey = customKey;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
     }
 }
