8451a1f23753b6368e20d2ae9ea94ae9e683f653
kaige zhong <kaige.zhong@isinonet.com>
2018-09-09 22:48:46 +0800

    blockingque and thread pool

---
+++ jdk/basic/src/main/java/com/collection/queue/SynchQueque.java
@@ -0,0 +1,51 @@
+package com.collection.queue;
+
+import java.util.concurrent.SynchronousQueue;
+
+public class SynchQueque {
+  public final static SynchronousQueue<String> QUEUE = new SynchronousQueue<>();
+
+  public static void main(String[] args) throws Exception {
+//    putAndTake();
+//    offer();
+
+  }
+
+  static void offer() throws InterruptedException {
+    SynchronousQueue<String> strings = new SynchronousQueue<>(true);
+    Thread producer = new Thread(() -> {
+      try {
+        String take = strings.take();
+        System.out.println(take);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+    boolean a = strings.offer("a");
+    System.out.println(a);
+    producer.start();
+    Thread.sleep(1000L);
+    boolean b = strings.offer("b");
+    System.out.println(b);
+  }
+  static void putAndTake() throws InterruptedException {
+    Thread producer = new Thread(() -> {
+      try {
+        QUEUE.put("put");
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+    Thread consumer = new Thread(() -> {
+      try {
+        String take = QUEUE.take();
+        System.out.println(take);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+    producer.start();
+    Thread.sleep(1000L);
+    consumer.start();
+  }
+}
---
+++ jdk/thread/src/main/java/com/ThreadPool.java
@@ -0,0 +1,45 @@
+package com;
+
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadPool {
+  private final AtomicInteger ctl = new AtomicInteger(ctlOf(RUNNING, 0));
+  private static final int COUNT_BITS = Integer.SIZE - 3;
+  private static final int CAPACITY   = (1 << COUNT_BITS) - 1;
+
+  // runState is stored in the high-order bits
+  private static final int RUNNING    = -1 << COUNT_BITS;
+  private static final int SHUTDOWN   =  0 << COUNT_BITS;
+  private static final int STOP       =  1 << COUNT_BITS;
+  private static final int TIDYING    =  2 << COUNT_BITS;
+  private static final int TERMINATED =  3 << COUNT_BITS;
+
+  private static int ctlOf(int rs, int wc) { return rs | wc; }
+  public static void main(String[] args) {
+    //java中int是有符号的
+    System.out.println(COUNT_BITS);
+    System.out.println(Integer.toBinaryString(CAPACITY));
+    System.out.println(Integer.toBinaryString(-1));
+    System.out.println(Integer.toBinaryString(RUNNING));
+    System.out.println(Integer.toBinaryString(SHUTDOWN));
+    System.out.println(Integer.toBinaryString(STOP));
+    System.out.println(Integer.toBinaryString(TIDYING));
+    System.out.println(Integer.toBinaryString(TERMINATED));
+
+    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2, 5, 100, TimeUnit.SECONDS,
+      new LinkedBlockingDeque<>());
+    threadPoolExecutor.execute(() -> {
+      System.out.println("aaa");
+    });
+    threadPoolExecutor.execute(() -> {
+      System.out.println("bbb");
+    });
+    threadPoolExecutor.execute(() -> {
+      System.out.println("ccc");
+    });
+  }
+
+}
