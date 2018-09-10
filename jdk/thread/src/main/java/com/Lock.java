6f639fc6d136a803706f3bb989d28f7468b19d01
Kaige Zhong <zhongkaige@xinchao.com>
2018-09-10 17:27:21 +0800

    a

---
+++ jdk/thread/src/main/java/com/Lock.java
@@ -0,0 +1,10 @@
+package com;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Lock {
+    public static void main(String[] args) {
+        ReentrantLock reentrantLock = new ReentrantLock();
+        reentrantLock.lock();
+    }
+}
