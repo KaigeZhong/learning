8d953df1c19a98ba06a638017c63d24be1fba393
Kaige Zhong <zhongkaige@xinchao.com>
2018-10-17 18:16:52 +0800

    json jackson

---
+++ java/spring-learning/spring-third/json/jackson/src/main/java/com/learning/spring/third/json/jackson/JacksonApp.java
@@ -0,0 +1,11 @@
+package com.learning.spring.third.json.jackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonApp {
+    public static void main(String[] args) {
+        SpringApplication.run(JacksonApp.class, args);
+    }
+}
---
+++ java/spring-learning/spring-third/json/jackson/src/main/java/com/learning/spring/third/json/jackson/ctrl/SampleCtrl.java
@@ -0,0 +1,14 @@
+package com.learning.spring.third.json.jackson.ctrl;
+
+import com.learning.spring.third.json.jackson.JacksonApp;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleCtrl {
+
+    public JacksonApp jacksonApp() {
+        JacksonApp jacksonApp = new JacksonApp();
+//        jacksonApp.
+        return null;
+    }
+}
