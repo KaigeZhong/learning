59fe546d0334af7d9421b69bf8aadc39543fd432
kaige zhong <kaige.zhong@isinonet.com>
2018-10-24 20:58:23 +0800

    jnin java

--- java/jdk/basic/src/main/java/com/jni/JavaJNI.java
+++ java/jdk/basic/src/main/java/com/jni/JavaJNI.java
@@ -8,17 +8,21 @@ package com.jni;
  * java com.jni.JavaJNI
  */
 public class JavaJNI {
+    public int age = 20;
+
     static{
         System.load("/home/kaige/work/research/learning/java/jdk/basic/src/main/java/javajni.so");
     }
-
     public static void main(String[] args)
     {
         JavaJNI javaJNI = new JavaJNI();
         javaJNI.printstr();
         javaJNI.printstr("wo shi ben di diao yong ce shi");
+        javaJNI.changeAge();
+        System.out.println("age after changed by native: " + javaJNI.age);
     }
     //native的作用是申明该方法在本地实现的，与Ｃ或Ｃ＋＋中的方法相对应
     native void printstr();
     native void printstr(String str);
+    native void changeAge();
 }
--- java/jdk/basic/src/main/java/javajni.c
+++ java/jdk/basic/src/main/java/javajni.c
@@ -16,3 +16,19 @@ JNIEXPORT void JNICALL Java_com_jni_JavaJNI_printstr__Ljava_lang_String_2
     const char *str = (*env)->GetStringUTFChars(env, string, 0);
     printf("%s!\n", str);
   }
+
+JNIEXPORT void JNICALL Java_com_jni_JavaJNI_changeAge
+  (JNIEnv *env, jobject obj)
+  {
+    //获致obj中对象的class
+    jclass clazz = (*env)->GetObjectClass(env, obj);
+    // 获取java中age字段的ID(最后一个参数是age的签名)
+    jfieldID id_age = (*env)->GetFieldID(env, clazz, "age", "I");
+    // 获取age字段对应的值
+    jint age = (*env)->GetIntField(env, obj, id_age);
+    // 输出
+    // printf("current age is: %d\n", number);
+    printf("age before changed: %d\n", age);
+    // 重新修改age字段的值为100。
+    (*env)->SetIntField(env, obj, id_age, 100);
+  }
