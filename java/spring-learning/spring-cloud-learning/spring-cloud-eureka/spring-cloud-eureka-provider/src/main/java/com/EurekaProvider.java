2a483a95c5bfcd7d04438ce58a7a7a1006a60d03
Kaige Zhong <zhongkaige@xinchao.com>
2018-10-29 15:32:39 +0800

    eureka

---
+++ java/spring-learning/spring-cloud-learning/spring-cloud-eureka/spring-cloud-eureka-provider/src/main/java/com/EurekaProvider.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * By having spring-cloud-starter-netflix-eureka-client on the classpath, your application automatically registers with the Eureka Server.
+ */
+@SpringBootApplication
+public class EurekaProvider {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaProvider.class, args);
+  }
+
+}
