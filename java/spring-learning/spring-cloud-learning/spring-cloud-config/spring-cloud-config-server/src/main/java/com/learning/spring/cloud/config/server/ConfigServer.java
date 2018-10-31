8fe132c3f95d41df0aac3b49a466111bc5aefb9a
Kaige Zhong <zhongkaige@xinchao.com>
2018-10-31 17:40:37 +0800

    bus

--- java/spring-learning/spring-cloud-learning/spring-cloud-config/spring-cloud-config-server/src/main/java/com/learning/spring/cloud/config/server/ConfigServer.java
+++ java/spring-learning/spring-cloud-learning/spring-cloud-config/spring-cloud-config-server/src/main/java/com/learning/spring/cloud/config/server/ConfigServer.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 查看server是否生效
+ * http://localhost:8888/applicationA/default
+ * http://localhost:8888/applicationA/dev
+ * 如果propertySources不为空且显示出配置文件内容，则表示server没有问题
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServer {
