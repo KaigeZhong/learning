4809b76c98a5726e524184d1c7d956b4c23132bc
Kaige Zhong <zhongkaige@xinchao.com>
2018-11-08 15:15:04 +0800

    spring cloud docker

---
+++ java/spring-learning/spring-cloud-learning/docker-spring-cloud/docker-config/docker-config-server/src/main/java/com/learning/spring/cloud/config/server/ConfigServer.java
@@ -0,0 +1,19 @@
+package com.learning.spring.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 查看server是否生效
+ * http://localhost:8888/application-a/default
+ * http://localhost:8888/application-a/dev
+ * 如果propertySources不为空且显示出配置文件内容，则表示server没有问题
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+}
