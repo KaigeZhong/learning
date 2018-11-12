a6df71254bffaeea69b1e256321aebb661eaa3cb
Kaige Zhong <zhongkaige@xinchao.com>
2018-11-12 18:22:34 +0800

    datasource

--- java/spring-learning/spring-third/mybatis/mybatis-xml/src/main/java/com/mybatis/xml/App.java
+++ java/spring-learning/spring-third/mybatis/mybatis-xml/src/main/java/com/mybatis/xml/App.java
@@ -2,13 +2,21 @@ package com.mybatis.xml;
 
 import com.mybatis.xml.dao.CityDao;
 import com.mybatis.xml.dao.DualDao;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * 直接在Mapper类上面添加注解@Mapper，这种方式要求每一个mapper类都需要添加此注解，麻烦。
+ * 通过使用@MapperScan可以指定要扫描的包的路径,
+ *@MapperScan(basePackages = {"com.lz.water.monitor.mapper"})
+ * 单个人觉得没必要用MapperScan
+ */
 @SpringBootApplication
+//@MapperScan(basePackages = {"com.lz.water.monitor.mapper"})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
